a2f72c0af15c941977003e18e00119ca6b550c7f
leonardo fleitas alvarez <leoflalv@gmail.com>
2022-07-25 17:03:15 +0200

    Add pokemon table view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import PokemonTable from 'views/pokemon-table';
+
 const App = () => {
   return (
     <div className="App">
       <h1>HKKKKKKKK HHH</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PokemonTable />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/views/pokemon-table/index.jsx
@@ -0,0 +1,9 @@
+const PokemonTable = () => {
+  return (
+    <div className="App">
+      <h1>Pokemon Table</h1>
+    </div>
+  );
+};
+
+export default PokemonTable;
